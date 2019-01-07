a2a2bec0d641fd9814963d1af09a7897f07f30e4
dstch <dstch@163.com>
2019-01-07 14:48:15 +0800

    add calculate code

--- word2vec_sim.py
+++ word2vec_sim.py
@@ -2,11 +2,14 @@ from gensim.models.word2vec import Word2Vec, LineSentence
 from gensim import corpora, models, similarities
 from data_helper import data_helper
 from distance_helper import distance_helper
+import jieba
+import numpy as np
 
 
 class word2vec_sim(object):
     def __init__(self):
-        pass
+        self.newwords = []
+        self.stopwords = []
 
     def word2vec_train(self, train_data_path, model_path):
         train_data = data_helper.get_train_data(train_data_path)
@@ -46,23 +49,43 @@ class word2vec_sim(object):
         model = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=num_topics)
         model.save(model_path)
 
-    def get_word_vector(self, text, word2vec_model, tfidf_model):
-        split_text1 = data_helper.split_sentence(text)
-        vector = []
-        text_len = 0
-        for word in split_text1:
-            if word in word2vec_model.wv.vocab:
-                text_len += 1
-                # add word weight
-                vector += word2vec_model[word] * tfidf_model[word]
-        vector /= text_len
-        return vector
+    def sentence_split(self, sentence):
+        try:
+            for newword in self.newwords:
+                jieba.add_word(newword)
+            sentence_seged = jieba.cut(sentence.strip())
+            outstr = ''
+            for word in sentence_seged:
+                word = word.lower()
+                if word not in self.stopwords:
+                    if word != '\t':
+                        outstr += word + " "
+            return outstr
+        except:
+            raise TypeError(sentence)
 
-    def similarity_word2vec_tfidf(self, text1, text2, word2vec_model, tfidf_model):
+    def get_word_vector(self, text, word2vec_model, tfidf_model, tfidf_dictionary):
+        split_text = [w for w in text if w in word2vec_model.wv.vocab]
+        vec = tfidf_dictionary.doc2bow(split_text)
+        vec_dict = dict(tfidf_model[vec])
+        tfidf_vec = []
+        for word in split_text:
+            tfidf_vec.append(vec_dict[tfidf_dictionary.token2id[word]])
+        tfidf_vec = np.array(tfidf_vec).T
+        word2vec_vec = word2vec_model[split_text].T
+        word2vec_tfidf_vec = np.dot(word2vec_vec, tfidf_vec)
+        return word2vec_tfidf_vec
+
+    def similarity_word2vec_tfidf(self, text1, text2, word2vec_model_path, tfidf_model_path, tfidf_dictionary_path):
         """
         calculate similarity between text1 and text2 by word2vec with TF-IDF weights
         :return:
         """
-        vector1 = self.get_word_vector(text1, word2vec_model, tfidf_model)
-        vector2 = self.get_word_vector(text2, word2vec_model, tfidf_model)
+        word2vec_model = Word2Vec.load(word2vec_model_path)
+        tfidf_dictionary = corpora.Dictionary.load(tfidf_dictionary_path)
+        tfidf_model = models.TfidfModel.load(tfidf_model_path)
+        text1 = self.sentence_split(text1).split()
+        text2 = self.sentence_split(text2).split()
+        vector1 = self.get_word_vector(text1, word2vec_model, tfidf_model, tfidf_dictionary)
+        vector2 = self.get_word_vector(text2, word2vec_model, tfidf_model, tfidf_dictionary)
         return distance_helper.cos_distance(vector1, vector2)
